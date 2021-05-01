c1036ba7edf1b358fbe190396c9c52b9ea952a55
jkaho <72959522+jkaho@users.noreply.github.com>
2021-05-01 23:25:10 +1000

    Add side menu to schedule and inspected properties pages

---
+++ client/src/pages/InspectedProperties/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import SideMenu from "../../components/SideMenu";
+
+export default function InspectedProperties() {
+  return (
+    <div>
+      <SideMenu />
+    </div>
+  );
+};
