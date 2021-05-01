b9a05663e006e642015ca369a441b200602c58d1
jkaho <72959522+jkaho@users.noreply.github.com>
2021-05-02 01:35:40 +1000

    Create notes sequelize model and sql table

--- models/notes.js
+++ models/notes.js
@@ -1,51 +1,22 @@
 // Creates User model
 module.exports = function(sequelize, DataTypes) {
-  const Review = sequelize.define("review", {
+  const Note = sequelize.define("note", {
     shared: {
       type: DataTypes.BOOLEAN,
       allowNull: false,
     },
-    propertyConditionRating: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 40]
+      }
     },
-    potentialRating: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    surroundingsRating: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    neighbourComparisonRating: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    accessibilityRating: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    privacyRating: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    floorplanRating: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    outdoorSpaceRating: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    indoorOutdoorFlowRating: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-    },
-    naturalLightRating: {
-      type: DataTypes.INTEGER,
+    text: {
+      type: DataTypes.STRING,
       allowNull: true,
     },
   });
 
-  return Review;
+  return Note;
 };
