8bddeed1e0081d11bb02772c82f5d55e50f87746
jkaho <72959522+jkaho@users.noreply.github.com>
2021-05-02 16:46:24 +1000

    Add commented out authentication related code for future reference

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+// Starts user session 
+/*
+  AUTHENTICATION
+  if (req.session.cookie === "authenticated") {
+    // render page
+  } 
+  
+  return res.redirect("/login");
+
+  COUNT VISIT 
+  if (req.session.countVisit) {
+    req.session.countVisit++;
+  } else {
+    req.session.countVisit = 1;
+  }
+
+*/
