ddb435ecacbc781a830959536bd2ca0602d9c6e5
jkaho <72959522+jkaho@users.noreply.github.com>
2021-05-02 16:13:50 +1000

    Install express session and set up server.js

---
+++ server.js
@@ -0,0 +1,41 @@
+// Require dependencies
+const express = require("express");
+const session = require("express-session");
+
+// Require models for syncing 
+const db = require("./models");;
+
+// Set up PORT 
+const PORT = process.env.PORT || 3001;
+
+// Create Express app 
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets (on Heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Set up sessions to keep track of user's login status
+app.use(
+  session({ secret: "happy donkey", resave: true, saveUninitialized: true })
+);
+
+// API Routes
+const routes = require("./routes");
+app.use(routes);
+
+// Send every other request to the React app
+const path = require("path")
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+// Sync database and log message upon success 
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+  });
+});
