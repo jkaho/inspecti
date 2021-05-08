a28e71fdf06098a71e259c7ab0e3fe57d694066b
jkaho <72959522+jkaho@users.noreply.github.com>
2021-05-08 19:17:23 +1000

    Customise rich text editor toolbar options

--- client/src/components/TextEditor/index.js
+++ client/src/components/TextEditor/index.js
@@ -36,6 +36,75 @@ function TextEditor() {
         wrapperClassName="wrapper-class"
         editorClassName="editor-class"
         toolbarClassName="toolbar-class"
+        toolbar={{
+          options: ['inline', 'list', 'emoji', 'image', 'link'],
+          inline: {
+            inDropdown: true,
+            className: undefined,
+            component: undefined,
+            dropdownClassName: undefined,
+            options: ['bold', 'italic', 'underline', 'strikethrough', 'monospace', 'superscript', 'subscript'],
+            bold: { className: undefined },
+            italic: { className: undefined },
+            underline: { className: undefined },
+            strikethrough: { className: undefined },
+            monospace: { className: undefined },
+            superscript: { className: undefined },
+            subscript: { className: undefined },
+          },
+          list: {
+            inDropdown: true,
+            className: undefined,
+            component: undefined,
+            dropdownClassName: undefined,
+            options: ['unordered', 'ordered'],
+          },
+          link: {
+            inDropdown: false,
+            className: undefined,
+            component: undefined,
+            popupClassName: undefined,
+            dropdownClassName: undefined,
+            showOpenOptionOnHover: true,
+            defaultTargetOption: '_self',
+            options: ['link', 'unlink'],
+            link: { className: undefined },
+            unlink: { className: undefined },
+            linkCallback: undefined
+          },
+          emoji: {
+            className: undefined,
+            component: undefined,
+            popupClassName: undefined,
+            emojis: [
+              '😀', '😁', '😂', '😃', '😉', '😋', '😎', '😍', '😗', '🤗', '🤔', '😣', '😫', '😴', '😌', '🤓',
+              '😛', '😜', '😠', '😇', '😷', '😈', '👻', '😺', '😸', '😹', '😻', '😼', '😽', '🙀', '🙈',
+              '🙉', '🙊', '👼', '👮', '🕵', '💂', '👳', '🎅', '👸', '👰', '👲', '🙍', '🙇', '🚶', '🏃', '💃',
+              '⛷', '🏂', '🏌', '🏄', '🚣', '🏊', '⛹', '🏋', '🚴', '👫', '💪', '👈', '👉', '👉', '👆', '🖕',
+              '👇', '🖖', '🤘', '🖐', '👌', '👍', '👎', '✊', '👊', '👏', '🙌', '🙏', '🐵', '🐶', '🐇', '🐥',
+              '🐸', '🐌', '🐛', '🐜', '🐝', '🍉', '🍄', '🍔', '🍤', '🍨', '🍪', '🎂', '🍰', '🍾', '🍷', '🍸',
+              '🍺', '🌍', '🚑', '⏰', '🌙', '🌝', '🌞', '⭐', '🌟', '🌠', '🌨', '🌩', '⛄', '🔥', '🎄', '🎈',
+              '🎉', '🎊', '🎁', '🎗', '🏀', '🏈', '🎲', '🔇', '🔈', '📣', '🔔', '🎵', '🎷', '💰', '🖊', '📅',
+              '✅', '❎', '💯',
+            ],
+          },
+          image: {
+            className: undefined,
+            component: undefined,
+            popupClassName: undefined,
+            urlEnabled: true,
+            uploadEnabled: true,
+            alignmentEnabled: true,
+            uploadCallback: undefined,
+            previewImage: false,
+            inputAccept: 'image/gif,image/jpeg,image/jpg,image/png,image/svg',
+            alt: { present: false, mandatory: false },
+            defaultSize: {
+              height: 'auto',
+              width: 'auto',
+            },
+          },
+        }}
       />
       <div className="preview" dangerouslySetInnerHTML={createMarkup(convertedContent)}></div>
     </div>
