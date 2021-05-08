3a7169185e007cec7b0ea671eaf1235cfb2128c4
jkaho <72959522+jkaho@users.noreply.github.com>
2021-05-09 01:22:03 +1000

    Add daily view & add event btn to calendar, add styles

--- client/src/pages/MonthlySchedule/index.js
+++ client/src/pages/MonthlySchedule/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import SideMenu from "../../components/SideMenu";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import "./style.css";
 // import MonthlyCalendar from "../../components/MonthlyCalendar";
@@ -17,9 +18,20 @@ export default function MonthlySchedule() {
       <div className="calendar-container">
         <div className="calendar-wrapper">
           <FullCalendar
-            plugins={[ dayGridPlugin, interactionPlugin ]}
+            plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin ]}
             initialView="dayGridMonth"
             dateClick={handleDateClick}
+            customButtons={{
+              eventButton: {
+                text: "ADD EVENT",
+                click: () => console.log("Add event!")
+              }
+            }}
+            headerToolbar={{
+              center: 'eventButton dayGridMonth,timeGridDay',
+              left: 'title',
+              right: 'prev,next',
+            }}
           />
         </div>
         {/* <MonthlyCalendar /> */}
