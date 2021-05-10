66cbc98ad69315deba4ef3e009c1a19a82733be4
jkaho <72959522+jkaho@users.noreply.github.com>
2021-05-10 18:55:55 +1000

    Move event edit/delete logic from event card to daily schedule page component, re-render page on edit/delete

--- client/src/pages/DailySchedule/index.js
+++ client/src/pages/DailySchedule/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import SideMenu from "../../components/SideMenu";
@@ -11,23 +11,88 @@ import { IconButton, ListItemSecondaryAction } from "@material-ui/core";
 import moment from "moment";
 import eventsAPI from "../../utils/eventsAPI";
 import EventCard from "../../components/EventCard";
+import clsx from "clsx";
+import Modal from "@material-ui/core/Modal";
+import TextField from "@material-ui/core/TextField";
+import FormControl from "@material-ui/core/FormControl";
+import PopupMessage from "../../components/PopupMessage";
 
-const useStyles = makeStyles({
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+};
+
+const useStyles = makeStyles((theme) => ({
   arrowButton: {
     padding: 0,
   },
-});
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+  margin: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1),
+  },
+  typeTextField: {
+    width: '18ch',
+  },
+  addressTextField: {
+    width: "42ch",
+  },
+  createButton: {
+    color: "rgb(255, 235, 255)",
+    background: "purple",
+    "&:hover": {
+      background: "rgb(99, 0, 99)",
+    }
+  },
+  cancelButton: {
+    color: "purple",
+    background: "rgb(255, 235, 255)",
+    "&:hover": {
+      background: "rgb(255, 225, 255)",
+    },
+    marginLeft: 20,
+  },
+  hide: {
+    display: "none",
+  },
+  show: {
+    display: "block",
+  },
+}));
 
 export default function DailySchedule(props) {
   const classes = useStyles();
   let { state } = useLocation();
   const [date, setDate] = useState();
   const [events, setEvents] = useState([]);
+  // getModalStyle is not a pure function, we roll the style only on the first render
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+  const [eventType, setEventType] = React.useState("Inspection");
+  const [addEventPopupIsOpen, setAddEventPopupState] = React.useState(false);
+  const [eventToModify, setEventToModify] = React.useState();
 
   useEffect(() => {
     setDate(state);
     getDailyEvents(state);
   }, []);
+
+  const typeRef = useRef();
+  const startTimeRef = useRef();
+  const endTimeRef = useRef();
 
   const getDailyEvents = (dateQuery) => {
     let startTime = moment(dateQuery).format("YYYY-MM-DD HH:mm:ss");
@@ -51,6 +116,110 @@ export default function DailySchedule(props) {
     setDate(state);
     getDailyEvents(state);
   };
+
+  const handleModalOpen = () => {
+    setOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setOpen(false);
+  };
+
+  const handleClose = () => {
+    setAddEventPopupState(false);
+  };
+
+  const handleEventTypeChange = (event) => {
+    setEventType(event.target.value);
+  };
+
+  const handleEditEventButtonClick = (event) => {
+    const cardId = event.target.id.split("-")[1];
+    setEventToModify(cardId);
+    handleModalOpen();
+  };
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    const updatedValues = {
+      eventType: typeRef.current.children[1].children[0].value,
+      startTime: startTimeRef.current.value,
+      endTime: endTimeRef.current.value,
+    };
+
+    console.log(updatedValues)
+
+    console.log(eventToModify)
+    eventsAPI.updateEvent(eventToModify, updatedValues)
+      .then(res => {
+        console.log(res);
+        handleModalClose();
+        setAddEventPopupState(true);
+        getDailyEvents(date);
+      })
+      .catch(err => console.log(err))
+  };
+
+  const handleDeleteButtonClick = (event) => {
+    const buttonId = event.target.id.split("-")[1];
+    eventsAPI.deleteEvent(buttonId)
+      .then(res => {
+        console.log(res);
+        getDailyEvents(date);
+      })
+      .catch(err => console.log(err))
+  };
+
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <h2 id="simple-modal-title">New property event</h2>
+      <form onSubmit={handleFormSubmit}>
+        <div className="event-type-div event-div">
+          <FormControl className={clsx(classes.margin, classes.typeTextField)} variant="outlined">
+            <TextField
+              ref={typeRef}
+              id="outlined-select-event-type"
+              select
+              label="Event type"
+              value={eventType}
+              onChange={handleEventTypeChange}
+              SelectProps={{
+                native: true,
+              }}
+              variant="outlined"
+            >
+              <option key="inspection-event-type" value="Inspection">
+                Inspection
+              </option>
+              <option key="auction-event-type" value="Auction">
+                Auction
+              </option>
+            </TextField>
+          </FormControl>
+        </div>
+        <div className="event-time-div event-div">
+          <label htmlFor="event-startTime">Start time</label><br/>
+          <input type="datetime-local" id="event-startTime"
+            ref={startTimeRef} noValidate
+            name="event-startTime" defaultValue={moment().format("yyyy-MM-DDThh:mm")} min={moment().format("yyyy-MM-DDThh:mm")}
+          />
+        </div>
+        <div className="event-time-div-2 event-div">
+          <label htmlFor="event-endTime">End time</label><br/>
+          <input type="datetime-local" id="event-endTime"
+            ref={endTimeRef} noValidate
+            name="event-endTime" defaultValue={moment().format("yyyy-MM-DDThh:mm")} min={moment().format("yyyy-MM-DDThh:mm")}
+          />
+        </div>
+        <div className="event-create-div event-div">
+          <Button className={classes.createButton} variant="contained" type="submit">UPDATE EVENT</Button>
+          <Button className={classes.cancelButton} variant="contained" onClick={handleModalClose}>CANCEL</Button>
+        </div>
+      </form>
+
+    </div>
+  );
+
 
   return (
     <div>
@@ -127,6 +296,8 @@ export default function DailySchedule(props) {
               bathrooms={item.bathrooms}
               carSpaces={item.carSpaces}
               landSize={item.landSize}
+              editClick={handleEditEventButtonClick}
+              deleteClick={handleDeleteButtonClick}
             />
           )) : 
           <div className="no-events">
@@ -135,6 +306,25 @@ export default function DailySchedule(props) {
           } 
         </div>
       </div>
+      <div>
+      {/* <button type="button" onClick={handleModalOpen}>
+        Open Modal
+      </button> */}
+      <Modal
+        open={open}
+        onClose={handleModalClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        {body}
+      </Modal>
+      <PopupMessage
+        open={addEventPopupIsOpen}
+        handleClose={handleClose}
+        severity="success"
+        message="Event successully updated!"
+      />
+    </div>
     </div>
   );
 };
