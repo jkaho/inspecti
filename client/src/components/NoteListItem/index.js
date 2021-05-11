88306a3f9bab8bda03352b5b038315d9b38b41de
jkaho <72959522+jkaho@users.noreply.github.com>
2021-05-12 00:37:59 +1000

    Style note list star and delete buttons, style note search input

--- client/src/components/NoteListItem/index.js
+++ client/src/components/NoteListItem/index.js
@@ -2,27 +2,44 @@ import React, { useState, useEffect } from "react";
 import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
+import "./style.css";
 
 export default function NoteListItem(props) {
   const [isStarred, setStarredState] = useState();
+  const [listItemHover, setListItemHover] = useState(false);
 
   useEffect(() => {
     setStarredState(props.noteIsStarred);
   }, [props.noteIsStarred]);
+
+  const handleListItemMouseEnter = () => {
+    setListItemHover(true);
+  };
+
+  const handleListItemMouseExit = () => {
+    setListItemHover(false);
+  };
   
   return (
     <div>
-      <ListItem id={`note-${props.noteId}`} button onClick={props.listItemOnClick}>
+      <ListItem className="list-item" id={`note-${props.noteId}`} button
+        onClick={props.listItemOnClick}
+        onMouseEnter={handleListItemMouseEnter}
+        onMouseLeave={handleListItemMouseExit}
+      >
         <ListItemText id={`notetitle-${props.noteId}`}
           primary={props.currentNoteId === props.noteId && props.sideTitle !== "" ? props.sideTitle
             : props.noteTitle
           } />
       </ListItem>
-      <div className="list-item-actions">
-        <button id={`deletebtn-${props.noteId}`} onClick={props.deleteOnClick}>
+      <div className={
+        `list-item-actions 
+        ${listItemHover ? "onHover" : "noHover"}`
+      }>
+        <button className="list-item-delete" id={`deletebtn-${props.noteId}`} onClick={props.deleteOnClick}>
           <i id={`deleteicon-${props.noteId}`} className="far fa-trash-alt" />
         </button>
-        <button id={`starbtn-${props.noteId}`} onClick={(event) => {
+        <button className="list-item-star" id={`starbtn-${props.noteId}`} onClick={(event) => {
             props.starOnClick(event, props.noteIsStarred);
             setStarredState(!isStarred);
           }}>
