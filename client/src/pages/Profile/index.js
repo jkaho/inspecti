373448195278dc3655b2e20641f178933fc1bf3e
jkaho <72959522+jkaho@users.noreply.github.com>
2021-05-15 01:31:56 +1000

    Install chart.js and render inspected properties bar chart to profile page

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,21 +1,100 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SideMenu from "../../components/SideMenu";
 import BoxContainer from "../../components/BoxContainer";
 import "./style.css";
-// import authenticationAPI from "../../utils/authenticationAPI";
+import { Bar } from 'react-chartjs-2';
+import moment from "moment";
+import propertiesAPI from "../../utils/propertiesAPI";
 
-export default function Profile() {
-  // useEffect(() => {
-  //   authenticateUser();
-  // }, []);
+const recentMonths = {
+  sixMonthsAgo: moment().add(-6, "M"),
+  fiveMonthsAgo: moment().add(-5, "M"),
+  fourMonthsAgo: moment().add(-4, "M"),
+  threeMonthsAgo: moment().add(-3, "M"),
+  twoMonthsAgo: moment().add(-2, "M"),
+  oneMonthAgo: moment().add(-1, "M"),
+  thisMonth: moment()
+}
 
-  // function authenticateUser() {
-  //   authenticationAPI.authenticated()
-  //     .then((res) => {
-  //       console.log(res)
-  //     })
-  //     .catch(err => console.log(err))
-  // }
+
+export default function Profile(props) {
+  const [state, setState] = useState({});
+
+  useEffect(() => {
+    let monthlyData = [[], [], [], [], [], [], []];
+    propertiesAPI.getAllProperties()
+      .then(res => {
+        res.data.forEach(item => {
+          let monthYearInspected = moment(item.dateInspected).format("MMYY");
+          if (monthYearInspected === recentMonths.sixMonthsAgo.format("MMYY")) {
+            monthlyData[0].push(item);
+          } else if (monthYearInspected === recentMonths.fiveMonthsAgo.format("MMYY")) {
+            monthlyData[1].push(item);
+          } else if (monthYearInspected === recentMonths.fourMonthsAgo.format("MMYY")) {
+            monthlyData[2].push(item);
+          } else if (monthYearInspected === recentMonths.threeMonthsAgo.format("MMYY")) {
+            monthlyData[3].push(item);
+          } else if (monthYearInspected === recentMonths.twoMonthsAgo.format("MMYY")) {
+            monthlyData[4].push(item);
+          } else if (monthYearInspected === recentMonths.oneMonthAgo.format("MMYY")) {
+            monthlyData[5].push(item);
+          } else {
+            monthlyData[6].push(item);
+          } 
+        });
+
+        setState({
+          labels: [
+            recentMonths.sixMonthsAgo.format("MMM"),
+            recentMonths.fiveMonthsAgo.format("MMM"),
+            recentMonths.fourMonthsAgo.format("MMM"),
+            recentMonths.threeMonthsAgo.format("MMM"),
+            recentMonths.twoMonthsAgo.format("MMM"),
+            recentMonths.oneMonthAgo.format("MMM"),
+            recentMonths.thisMonth.format("MMM")
+          ],
+          datasets: [
+            {
+              label: 'Properties',
+              backgroundColor: 'rgba(75,192,192,1)',
+              borderColor: 'rgba(0,0,0,1)',
+              borderWidth: 2,
+              data: [
+                monthlyData[0].length,
+                monthlyData[1].length, 
+                monthlyData[2].length, 
+                monthlyData[3].length, 
+                monthlyData[4].length,
+                monthlyData[5].length,
+                monthlyData[6].length
+              ]
+            }
+          ]
+        })
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  function PropertiesChart() {
+    return (
+      <div>
+        <Bar
+          data={state}
+          options={{
+            title: {
+              display: true,
+              text: 'Properties inspected in the past 6 months',
+              fontSize: 20
+            },
+            legend: {
+              display: true,
+              position: 'right'
+            }
+          }}
+        />
+      </div>
+    )
+  };
 
   return (
     <div>
@@ -73,7 +152,7 @@ export default function Profile() {
                     </div>
                   </div>
                   <div className="chart-container">
-                    chart.js goes here
+                    <PropertiesChart />
                   </div>
                 </td>
               </tr>
