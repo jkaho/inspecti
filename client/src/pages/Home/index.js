4c3b2fffd657a8af0d6aac7646ee43a90b60267d
jkaho <72959522+jkaho@users.noreply.github.com>
2021-05-16 01:09:21 +1000

    Replace homepage location autocomplete with material design autocomplete component

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,7 +1,11 @@
 import React, { useRef, useState } from "react";
 import NavBar from "../../components/NavBar";
-import SearchBar from "../../components/SearchBar";
-import SuggestionMenu from "../../components/SuggestionMenu";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import TextField from '@material-ui/core/TextField';
+
+// import SearchAutocomplete from "../../components/SearchAutocomplete";
+// import SearchBar from "../../components/SearchBar";
+// import SuggestionMenu from "../../components/SuggestionMenu";
 import FilterDiv from "../../components/FilterDiv";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
@@ -31,54 +35,84 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
+function formatLocationSuggestion(obj) {
+  let location = `${obj.name}, ${obj.state.toUpperCase()}`;
+  if (obj.type === "suburb") {
+    location += ` ${obj.postcode}`;
+  } 
+
+  return location;
+};
+
+
 export default function Home() {
   const classes = useStyles();
+  // States
   const searchRef = useRef();
   // const [search, setSearch] = useState("");
-  const [suggestions, setSuggestions] = useState([]);
-  const [suggestionOpen, setSuggestionOpen] = useState(false);
-  const [search, setSearch] = useState("");
-
-  const handleInputChange = () => {
-    const query = searchRef.current.value;
-    setSearch(query);
-    setSuggestions([]);
-    if (query !== "") {
-      domainAPI.getLocationSuggestions(query)
+  const [locationSuggestions, setLocationSuggestions] = useState([]);
+  const [location, setLocation] = useState();
+  // const [suggestionOpen, setSuggestionOpen] = useState(false);
+  // const [search, setSearch] = useState("");
+  // Refs
+  const locationRef = useRef();
+  // const handleInputChange = () => {
+  //   const query = searchRef.current.value;
+  //   setSearch(query);
+  //   setSuggestions([]);
+  //   if (query !== "") {
+  //     domainAPI.getLocationSuggestions(query)
+  //     .then(res => {
+  //       setSuggestions(res.data);
+  //       if (res.data.length > 0) {
+  //         setSuggestionOpen(true);
+  //       } else {
+  //         setSuggestionOpen(false);
+  //       }
+  //     })
+  //     .catch(err => console.log(err));
+  //   }
+  // };
+  const handleLocationInputChange = () => {
+    const newValue = locationRef.current.children[0].children[1].children[0].value;
+    if (newValue === "") {
+      setLocationSuggestions([]);
+    } else {
+      domainAPI.getLocationSuggestions(newValue)
       .then(res => {
-        setSuggestions(res.data);
-        if (res.data.length > 0) {
-          setSuggestionOpen(true);
-        } else {
-          setSuggestionOpen(false);
-        }
+        setLocationSuggestions(res.data.splice(0, 10));
       })
-      .catch(err => console.log(err));
+      .catch(err => console.log(err))
     }
+  };
+
+  const handleSuggestionClick = (value) => {
+    setLocation(value);
+    console.log(value);
   };
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
-    setSuggestionOpen(false);
-    setSuggestions([]);
+    // setSuggestionOpen(false);
+    setLocationSuggestions([]);
     // API.getPropertyListings(search)
     //   .then(res => console.log(res.data))
     //   .catch(err => console.log(err))
   };
 
-  const handleLocationSuggestionClick = (event) => {
-    event.stopPropagation();
-    const locationEl = event.target;
-    let locationQuery = "";
-    if (locationEl.value) {
-      locationQuery += locationEl.value.trim();
-    } else {
-      locationQuery += locationEl.textContent.trim();
-    }
+  // const handleLocationSuggestionClick = (event) => {
+  //   event.stopPropagation();
+  //   const locationEl = event.target;
+  //   let locationQuery = "";
+  //   if (locationEl.value) {
+  //     locationQuery += locationEl.value.trim();
+  //   } else {
+  //     locationQuery += locationEl.textContent.trim();
+  //   }
 
-    setSearch(locationQuery);
-    searchRef.current.value = locationQuery;
-  };
+  //   setSearch(locationQuery);
+  //   searchRef.current.value = locationQuery;
+  // };
 
   return (
     <div className={classes.root}>
@@ -86,17 +120,33 @@ export default function Home() {
       <Grid item xs={12}>
         <div className="bg">
           <Typography variant="h4" className={classes.heading}>Search properties for sale</Typography>
-          <SearchBar
+          {/* <SearchBar
             placeholder="Search by suburb, state or postcode"
             inputRef={searchRef}
             onSubmit={handleFormSubmit}
             onChange={handleInputChange}
-          />
-          <SuggestionMenu
+          /> */}
+          {/* <SuggestionMenu
             suggestions={suggestions}
             open={suggestionOpen}
             onClick={handleLocationSuggestionClick}
-          />
+          /> */}
+          <form onSubmit={handleFormSubmit}>
+            <div className="location-autocomplete-wrapper">
+              <Autocomplete
+                id="location-autocomplete"
+                freeSolo
+                ref={locationRef}
+                onInputChange={handleLocationInputChange}
+                onChange={(event, value) => handleSuggestionClick(value)}
+                options={locationSuggestions}
+                getOptionLabel={(option) => formatLocationSuggestion(option)}
+                style={{ width: 300, padding: 0 }}
+                renderInput={(params) => <TextField {...params} label="Search for an address" variant="outlined"
+                />}
+              />
+            </div>
+          </form>
           <FilterDiv />
         </div>
       </Grid>
