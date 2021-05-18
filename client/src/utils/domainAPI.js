049abf781a67c6e6b86151e6f3688ff45dee067a
jkaho <72959522+jkaho@users.noreply.github.com>
2021-05-18 18:32:50 +1000

    Change domain API routes back to frontend temporarily

--- client/src/utils/domainAPI.js
+++ client/src/utils/domainAPI.js
@@ -2,35 +2,35 @@ import axios from "axios";
 const url = "http://localhost:3001";
 
 const domainAPI = {
-  // getLocationSuggestions: function(query) {
-  //   return axios.get(
-  //     "https://api.domain.com.au/v1/listings/locations?terms=" + query + "&pageNumber=1&pageSize=10",
-  //     { 
-  //       headers: {
-  //         "X-Api-Key": "key_dc635e3e95b67de404f3b44abbcc09e2"
-  //       }
-  //     }
-  //   );
-  // },
   getLocationSuggestions: function(query) {
-    return axios.get("/api/user/domain/location/q=" + query);
+    return axios.get(
+      "https://api.domain.com.au/v1/listings/locations?terms=" + query + "&pageNumber=1&pageSize=10",
+      { 
+        headers: {
+          "X-Api-Key": "key_dc635e3e95b67de404f3b44abbcc09e2"
+        }
+      }
+    );
   },
-  // getAddressSuggestions: function(query) {
-  //   return axios.get("https://api.domain.com.au/v1/properties/_suggest?terms=" + query + "&channel=Residential",
-  //     { 
-  //       headers: {
-  //         "X-Api-Key": "key_dc635e3e95b67de404f3b44abbcc09e2"
-  //       }
-  //     }
-  //   );
+  // getLocationSuggestions: function(query) {
+  //   return axios.get("/api/user/domain/location/q=" + query);
   // },
   getAddressSuggestions: function(query) {
-    axios.get("/api/user/domain/address/q=" + query)
-      .then(result => {
-        return result;
-      })
-      .catch(err => console.log(err))
+    return axios.get("https://api.domain.com.au/v1/properties/_suggest?terms=" + query + "&channel=Residential",
+      { 
+        headers: {
+          "X-Api-Key": "key_dc635e3e95b67de404f3b44abbcc09e2"
+        }
+      }
+    );
   },
+  // getAddressSuggestions: function(query) {
+  //   axios.get("/api/user/domain/address/q=" + query)
+  //     .then(result => {
+  //       return result;
+  //     })
+  //     .catch(err => console.log(err))
+  // },
   // getAddressSuggestions: function(query) {
   //   axios.get("/api/user/API_KEY")
   //     .then(result => {
@@ -44,18 +44,18 @@ const domainAPI = {
   //     );
   //     })
   // },
-  // getPropertyInfo: function(propertyId) {
-  //   return axios.get("https://api.domain.com.au/v1/properties/" + propertyId,
-  //     { 
-  //       headers: {
-  //         "X-Api-Key": "key_dc635e3e95b67de404f3b44abbcc09e2"
-  //       }
-  //     }
-  //   );
-  // }
   getPropertyInfo: function(propertyId) {
-    return axios.get("/api/user/domain/property/q=" + propertyId);
+    return axios.get("https://api.domain.com.au/v1/properties/" + propertyId,
+      { 
+        headers: {
+          "X-Api-Key": "key_dc635e3e95b67de404f3b44abbcc09e2"
+        }
+      }
+    );
   }
+  // getPropertyInfo: function(propertyId) {
+  //   return axios.get("/api/user/domain/property/q=" + propertyId);
+  // }
   // getPropertyListings: function(query) {
   //   return axios.post(
   //     "https://api.domain.com.au/v1/listings/residential/_search",
